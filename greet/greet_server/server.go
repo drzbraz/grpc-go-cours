be5ed7daa72be87e2187662a4428f68cfb679885
Daniel Braz <drz.braz@gmail.com>
2022-04-04 08:51:39 -0300

    feat(unary): add grpc unary channel

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
-import {
+import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -8,12 +9,22 @@ import {
 	"github.com/drzbraz/grpc-go-course/greet/greetpb"
 
 	"google.golang.org/grpc"
-}
+)
 
 type server struct{}
 
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
 func main(){
-	fmt.Println("Hello world")
+    fmt.Println("Hello world")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 
